b066428052619d81744860c822984807c6bc6c9e
wanxiturro <lbpf2021jaca@gmail.com>
2026-05-04 09:58:01 -0400

    Update: Exchange rates from Bs to USD / Voting to determine if the dollar will rise or fall.

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,14 @@ import 'screens/splash_screen.dart';
 import 'data/services/tasa_service.dart';
 import 'package:provider/provider.dart';
 import 'data/providers/theme_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'data/firebase/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await TasaService().initCustomRates(); 
   runApp(
     ChangeNotifierProvider(
